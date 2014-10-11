b375a577d687f7213a013386266ddb0a85b3da98
Carlos Lomeli <Carlos Lomeli>
2014-10-10 22:19:26 -0430

    Primera clase

---
+++ prueba.py
@@ -0,0 +1,17 @@
+def mi_funcion():
+    print "Hola mundo"
+
+a = mi_funcion()
+
+print a
+
+
+
+def nombres(nombre, apellido):
+	nombre_completo = "Mi nombre es %s %s y %d" % nombre, apellido, edad
+	print nombre_completo
+
+nombres("Carlos", "Lomeli")
+
+
+
