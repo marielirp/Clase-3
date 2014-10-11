bf5184d93860400a7f410d9d011140fd50d87352
Carlos Lomeli <Carlos Lomeli>
2014-10-10 22:31:40 -0430

    Añadido parametro a la funcion

--- prueba.py
+++ prueba.py
@@ -5,13 +5,11 @@ a = mi_funcion()
 
 print a
 
-
-
-def nombres(nombre, apellido):
-	nombre_completo = "Mi nombre es %s %s y %d" % nombre, apellido, edad
+def nombres(nombre, apellido, edad):
+	nombre_completo = "Mi nombre es %s %s y %d" %(nombre, apellido, edad)
 	print nombre_completo
 
-nombres("Carlos", "Lomeli")
+nombres("Carlos", "Lomeli", 25)
 
 
 
